f9299299846a2e5455b042302a77218e768e2bc2
--global <--global>
2025-01-16 15:26:22 -0300

    GET Products paged

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -3,10 +3,14 @@ package com.devsuperior.dscommerce.controllers;
 import com.devsuperior.dscommerce.Services.ProductService;
 import com.devsuperior.dscommerce.dto.ProductDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -15,10 +19,17 @@ public class ProductController {
     @Autowired
     private ProductService service;
 
+    //GET ONE
     @GetMapping(value = "/{id}")
     public ProductDTO findById(@PathVariable Long id) {
         ProductDTO dto = service.findById(id);
         return dto;
+    }
+
+    //GET ALL
+    @GetMapping
+    public Page<ProductDTO> findByAll(Pageable pageable) {
+        return service.findAll(pageable);
     }
 
 }
